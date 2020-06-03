b52203da5fe363beb52e084a177acc5375648863
mr147sec <busiyg@163.com>
2020-06-03 16:33:35 +0800

    Init Project add show FPS

---
+++ Assets/Scenes/ShowFPS/ShowFPS.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowFPS : MonoBehaviour
+{
+    public int fpsTarget;
+    public float updateInterval = 0.5f;
+    private float lastInterval;
+    private int frames = 0;
+    private float fps;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //设置帧率
+        Application.targetFrameRate = fpsTarget;
+        lastInterval = Time.realtimeSinceStartup;
+        frames = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ++frames;
+        float timeNow = Time.realtimeSinceStartup;
+        if (timeNow >= lastInterval + updateInterval)
+        {
+            fps = frames / (timeNow - lastInterval);
+            frames = 0;
+            lastInterval = timeNow;
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 30), fps.ToString());
+    }
+}
